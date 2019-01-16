2b176c530b12975657dc1bc323a2a6632b301511
lizhooh <960316408@qq.com>
2019-01-16 13:04:52 +0800

    update：async load

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import loadable from '@loadable/component';
 import Index from '../views/Index';
 import LoadPageIcon from '../components/common/LoadPageIcon';
+import loadable from 'react-loadable';
 
 const asyncLoad = (C) => {
-    const View = loadable(() => C, { fallback: <LoadPageIcon /> });
+    const View = loadable({
+        loader: () => C,
+        loading: LoadPageIcon,
+    });
     return (props) => <View {...props} />;
 }
 
